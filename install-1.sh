0b9b9d5d3ef981e3532fb5cdbf20f430648da5d3
Aaron-Mann <aaron.mann@outlook.com>
2020-10-23 15:28:55 +0000

    install fixes

--- install-1.sh
+++ install-1.sh
@@ -16,27 +16,15 @@ do
 	esac
 done
 
-while [ $pause2 == 1 ]
-do
-	echo "Enter a password for the $USER account:"
-	read PSWD
-	read -p "You have chosen $PSWD for your password. Are you sure? [y/N]" yn
-	case $yn in
-		[Yy]* ) pause2=0 ;;
-		[yes]* ) pause2=0 ;;
-		[Yes]* ) pause2=0 ;;
-	esac
-done
-
 useradd -m -g wheel -s /bin/bash $USER
-passwd $USER $PSWD
+passwd $USER
 
 echo "User Account Configured."
 
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 echo "Wheel group no longer needs root password to execute sudo."
-
+pacman --noconfirm -Syu
 pacman --noconfirm -Syy xonotic networkmanager ripgrep sudo git base-devel wget curl xorg xorg-xinit xwallpaper picom alacritty vim emacs alsa alsa-utils neofetch python python-pip xmonad xmonad-contrib neovim
 
 systemctl enable NetworkManager
@@ -46,6 +34,6 @@ cp install-2.sh /home/$USER/install-2.sh
 
 echo "Root system level configuration has been completed."
 echo " "
-echo "Please log out of root (with ctrl-d), and log into your system as '$USER' with the password '$PSWD' and run the following command:"
+echo "Please log out of root (with ctrl-d), and log into your system as '$USER' and run the following command:"
 echo ""
 echo "bash install-2.sh"
