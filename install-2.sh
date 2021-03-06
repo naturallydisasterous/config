ae8f569bd9f1846e02473157ca104217c32fb377
Aaron-Mann <aaron.mann@outlook.com>
2021-03-06 00:34:30 -0800

    Changes

--- install-2.sh
+++ install-2.sh
@@ -4,18 +4,6 @@ path=$(pwd)
 
 echo "Beginning Installation..."
 
-echo "Cloning mimicwm Repository..."
-
-git clone https://github.com/Aaron-Mann/mimicwm
-
-echo "Building mimicwm..."
-
-cd mimicwm
-
-sudo make clean install
-
-cd ..
-
 echo "Cloning yay Repository..."
 
 git clone https://aur.archlinux.org/yay.git
@@ -32,15 +20,7 @@ rm -rf yay
 
 echo "Installing AUR Packages..."
 
-yay --noconfirm -S brave-bin minecraft-launcher discord
-
-echo "Configuring Emacs..."
-
-# configure doom emacs
-
-git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-
-~/.emacs.d/bin/doom install
+yay --noconfirm -S leftwm brave-bin minecraft-launcher discord
 
 # configure dotfiles and configs
 
@@ -52,7 +32,7 @@ echo "Creating .xinitrc file..."
 
 echo "xwallpaper --zoom $HOME/.wallpaper.jpg" >> $HOME/.xinitrc
 echo "picom &" >> $HOME/.xinitrc
-echo "exec mimicwm" >> $HOME/.xinitrc
+echo "exec dbus-launch leftwm" >> $HOME/.xinitrc
 
 echo "Creating .bashrc file..."
 
@@ -65,11 +45,8 @@ echo "Creating Symlinks to '.config' Directory..."
 [ -d $HOME/.config/alacritty ] && rm -rf $HOME/.config/alacritty
 cp -r $path/config/config/alacritty $HOME/.config/alacritty
 
-[ -d $HOME/.config/nvim ] && rm -rf $HOME/.config/nvim
-cp -r $path/config/config/nvim $HOME/.config/nvim
-
 cp -r $path/config/.wallpaper.jpg $HOME/.wallpaper.jpg
 
 echo "Congragulations! Installation is complete!"
 echo ""
-echo "Now, type 'startx' to enter xmonad and begin computing!"
+echo "Now, type 'startx' to enter leftwm and begin computing!"
